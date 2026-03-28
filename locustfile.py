72861ca4679b2fc33fb99ce4fbe68ff291c3501c
FanniFarkas <farkasfanni@edu.bme.hu>
2026-03-28 12:52:22 +0200

    Create locustfille

---
+++ locustfile.py
@@ -0,0 +1,17 @@
+from locust import HttpUser, task, between
+
+class PhotoAlbumUser(HttpUser):
+    wait_time = between(1, 2) # 1-2 másodperces szünet a kérések között
+
+    @task(3)
+    def view_home(self):
+        self.client.get("/")
+
+    @task(2)
+    def view_photolist(self):
+        # Feltételezve, hogy ez az albumod elérési útja
+        self.client.get("/photolist/")
+
+    @task(1)
+    def check_login_page(self):
+        self.client.get("/users/login/")
